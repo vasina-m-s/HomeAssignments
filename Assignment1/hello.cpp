d9a10bed2eb99124e998100825286b3e9cb2102a
Marina Vasina <st140082@student.spbu.ru>
2025-10-05 19:21:51 +0300

    transfer to a folder

---
+++ Assignment1/hello.cpp
@@ -0,0 +1,12 @@
+#include "hello.h"
+
+#include <iostream>
+
+void myproject :: sayHello () {
+	std::cout << "Hello, world" << std::endl;
+	std::string s;
+	while (true){
+		std::cin >> s;
+		std::cout << "Hello, " << s << "!" << std::endl;
+	}
+}
